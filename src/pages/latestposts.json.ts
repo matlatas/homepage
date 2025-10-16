13873779a58694a53d4a023a14186bf79eb78afc
Kjell Haaland <kjell@kjellhaaland.com>
2025-10-16 15:14:25 +0200

    Fixed latest posts

--- src/pages/latestposts.json.ts
+++ src/pages/latestposts.json.ts
@@ -1,20 +1,19 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
-export const GET: APIRoute = async ({params, request}) => {
+export const GET: APIRoute = async ({ params, request }) => {
+  // Filter blog entries with 'draft: false' & date before current date
+  const publishedBlogEntries = await getCollection("blog", ({ id, data }) => {
+    return !data.draft && id !== "-index.md";
+  });
 
-    // Filter blog entries with 'draft: false' & date before current date
-    const publishedBlogEntries = await getCollection("blog", ({ data }) => {
-        return !data.draft && data.publishDate < new Date();
-    });
+  // Sort by date in descending order
+  publishedBlogEntries.sort((a, b) => {
+    return b.data.publishDate - a.data.publishDate;
+  });
 
-    // Sort by date in descending order
-    publishedBlogEntries.sort((a, b) => {
-        return b.data.publishDate.getTime() - a.data.publishDate.getTime();
-    });
+  // Limit to 5 blog entries
+  const firstFiveBlogEntries = publishedBlogEntries.slice(0, 5);
 
-    // Limit to 5 blog entries
-    const firstFiveBlogEntries = publishedBlogEntries.slice(0, 5);
-
-    return new Response(JSON.stringify(firstFiveBlogEntries));
-}
+  return new Response(JSON.stringify(firstFiveBlogEntries));
+};
