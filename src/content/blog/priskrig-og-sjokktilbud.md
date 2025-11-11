ab863f2706c461d373a91a17d2881381405f5987
Kjell Haaland <kjell@kjellhaaland.com>
2025-11-11 08:52:13 +0100

    Updated with pricewarposts

---
+++ src/content/blog/priskrig-og-sjokktilbud.md
@@ -0,0 +1,10 @@
+---
+draft: false
+title: "Priskrig og sjokktilbud"
+description: "Kommer snart: Oversikt over priskriger og sjokktilbud!"
+image: "/images/logo.png"
+date: "2025-10-16 08:00"
+---
+
+Kommer snart: Oversikt over priskriger og sjokktilbud!
+Følg med for oppdateringer!
---
+++ src/pages/pricewarpost.json.ts
@@ -0,0 +1,25 @@
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+export const GET: APIRoute = async ({ params, request }) => {
+  // Filter blog entries with 'draft: false' & date before current date
+  const publishedBlogEntries = await getCollection("blog", ({ id, data }) => {
+    return !data.draft && id === "priskrig-og-sjokktilbud";
+  });
+
+  // Limit to 5 blog entries
+  const entry = publishedBlogEntries[0];
+
+  const res = {
+    id: entry.id,
+    slug: entry.id,
+    isActive: true,
+    data: {
+      title: entry.data.title,
+      snippet: entry.data.description,
+      image: { src: entry.data.image },
+    },
+  };
+
+  return new Response(JSON.stringify(res));
+};
